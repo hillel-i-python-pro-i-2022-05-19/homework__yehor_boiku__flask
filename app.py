29be81f9f4832ec04ff6648e48bd9d4bb5ba1e27
yehor <yehorboiku@gmail.com>
2022-06-04 17:21:32 +0300

    v.1.0.0

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask
+import requests
+import pandas as pd
+from create_fake_users import fake_name_and_email
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello Aleksanr, it\'s my homework'
+
+
+@app.route('/requirements')
+def requirements():
+    with open('anacondaz.txt', 'r') as file:
+        song = str(file.read())
+        song_1 = song.split('\n')
+        return "<p>".join(song_1)
+
+
+@app.route('/generate-users/<int:number>')
+def create_name_and_email(number):
+    return fake_name_and_email(number)
+
+
+@app.route('/space')
+def astronaut():
+    number_of_astronauts = requests.get('http://api.open-notify.org/astros.json')
+    return f'Number of astronauts is {str(number_of_astronauts.json()["number"])}'
+
+
+@app.route('/mean')
+def average_height_and_weight():
+    data = pd.read_csv('people_data.csv')
+    average_height_in_cm = round(((data[' "Height(Inches)"'].mean()) * 2.54), 2)
+    average_weight_in_kg = round(((data[' "Weight(Pounds)"'].mean()) / 2.205), 2)
+    return f"<p>Average height: {average_height_in_cm} cm.</p> <p>Average weight: {average_weight_in_kg} kg.</p>"
+
+
+if __name__ == '__main__':
+    app.run()
